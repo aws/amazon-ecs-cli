2bc73fc7afb4f04c2509c1d3f52844064f260bb7
Pieter Ennes <skion@users.noreply.github.com>
2016-01-07 15:23:49 +0000

    Update AMIs from 2015.09.b to 2015.09.c.
    Fixes #56.

--- ecs-cli/modules/config/ami/ami.go
+++ ecs-cli/modules/config/ami/ami.go
@@ -29,12 +29,12 @@ type staticAmiIds struct {
 func NewStaticAmiIds() ECSAmiIds {
 	regionToId := make(map[string]string)
 	// amzn-ami-2015.09.b-amazon-ecs-optimized AMIs
-	regionToId["us-east-1"] = "ami-ddc7b6b7"
-	regionToId["us-west-1"] = "ami-a39df1c3"
-	regionToId["us-west-2"] = "ami-d74357b6"
-	regionToId["eu-west-1"] = "ami-f1b46b82"
-	regionToId["ap-northeast-1"] = "ami-3077525e"
-	regionToId["ap-southeast-2"] = "ami-23b4eb40"
+	regionToId["us-east-1"] = "ami-6ff4bd05"
+	regionToId["us-west-1"] = "ami-46cda526"
+	regionToId["us-west-2"] = "ami-313d2150"
+	regionToId["eu-west-1"] = "ami-8073d3f3"
+	regionToId["ap-northeast-1"] = "ami-6ca38b02"
+	regionToId["ap-southeast-2"] = "ami-00e7bf63"
 
 	return &staticAmiIds{regionToId: regionToId}
 }
