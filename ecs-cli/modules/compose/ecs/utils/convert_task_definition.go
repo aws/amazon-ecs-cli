52e1e2aaac2dc951a270a1be00b34142fca68a7d
Alex Carver <alexander.carver@gmail.com>
2016-01-20 11:19:42 +0000

    Update convert_task_definition.go

--- ecs-cli/modules/compose/ecs/utils/convert_task_definition.go
+++ ecs-cli/modules/compose/ecs/utils/convert_task_definition.go
@@ -144,7 +144,7 @@ func convertToContainerDef(inputCfg *libcompose.ServiceConfig,
 	outputContDef.EntryPoint = aws.StringSlice(inputCfg.Entrypoint.Slice())
 	outputContDef.Environment = environment
 	if inputCfg.Restart == "no" {
-		outputContDef.Essential = aws.Bool(true)
+		outputContDef.Essential = aws.Bool(false)
 	}
 	outputContDef.ExtraHosts = extraHosts
 	if inputCfg.Hostname != "" {
