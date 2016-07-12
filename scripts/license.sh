3048fe9b9be01bb629dba850527c05627dd10fd0
uttarasridhar <uttara@amazon.com>
2016-07-12 12:55:51 -0700

    Update copyright year

--- scripts/license.sh
+++ scripts/license.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may
 # not use this file except in compliance with the License. A copy of the
@@ -30,7 +30,7 @@ for user in ./../vendor/github.com/*; do
 done;
 
 cat << EOF > "${outputfile}"
-// Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
