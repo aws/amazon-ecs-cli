b9a36ba6739945eb2bea77dc61fdcffee0996e52
Wesley Pettit <wppttt@amazon.com>
2017-10-16 15:39:27 -0700

    Added --aws-profile flag.

--- ecs-cli/modules/commands/flags.go
+++ ecs-cli/modules/commands/flags.go
@@ -35,6 +35,7 @@ const (
 	ProfileConfigFlag      = "ecs-profile"
 	ProfileNameFlag        = "profile-name"
 	ConfigNameFlag         = "config-name"
+	AWSProfileNameFlag     = "aws-profile"
 
 	ComposeProjectNamePrefixFlag         = "compose-project-name-prefix"
 	ComposeProjectNamePrefixDefaultValue = "ecscompose-"
@@ -85,6 +86,7 @@ const (
 // OptionalRegionFlag inline overrides region
 // OptionalClusterConfigFlag specifies the cluster profile to read from config
 // OptionalProfileConfigFlag specifies the credentials profile to read from the config
+// OptionalAWSProfileFlag specifies the AWS Profile to use for credential information
 func OptionalRegionAndProfileFlags() []cli.Flag {
 	return []cli.Flag{
 		cli.StringFlag{
@@ -103,6 +105,12 @@ func OptionalRegionAndProfileFlags() []cli.Flag {
 			Name: ProfileConfigFlag,
 			Usage: fmt.Sprintf(
 				"[Optional] Specifies the name of the ECS profle configuration to use. Defaults to the default profile configuration.",
+			),
+		},
+		cli.StringFlag{
+			Name: AWSProfileNameFlag,
+			Usage: fmt.Sprintf(
+				"[Optional] Use your AWS credentials from an existing named profile in ~/.aws/credentials.",
 			),
 		},
 	}
