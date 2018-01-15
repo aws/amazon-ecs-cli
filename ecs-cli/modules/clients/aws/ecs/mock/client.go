a93484b67e2bed4954eeffd803311ac4a7f279ee
Allison Stanko <stankoa@amazon.com>
2018-01-15 18:37:24 +0000

    Add Force flag to client, regen mocks for ecs client

--- ecs-cli/modules/clients/aws/ecs/mock/client.go
+++ ecs-cli/modules/clients/aws/ecs/mock/client.go
@@ -214,22 +214,22 @@ func (_mr *_MockECSClientRecorder) StopTask(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "StopTask", arg0)
 }
 
-func (_m *MockECSClient) UpdateService(_param0 string, _param1 string, _param2 int64, _param3 *ecs.DeploymentConfiguration, _param4 *ecs.NetworkConfiguration, _param5 *int64) error {
-	ret := _m.ctrl.Call(_m, "UpdateService", _param0, _param1, _param2, _param3, _param4, _param5)
+func (_m *MockECSClient) UpdateService(_param0 string, _param1 string, _param2 int64, _param3 *ecs.DeploymentConfiguration, _param4 *ecs.NetworkConfiguration, _param5 *int64, _param6 bool) error {
+	ret := _m.ctrl.Call(_m, "UpdateService", _param0, _param1, _param2, _param3, _param4, _param5, _param6)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
-func (_mr *_MockECSClientRecorder) UpdateService(arg0, arg1, arg2, arg3, arg4, arg5 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "UpdateService", arg0, arg1, arg2, arg3, arg4, arg5)
+func (_mr *_MockECSClientRecorder) UpdateService(arg0, arg1, arg2, arg3, arg4, arg5, arg6 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "UpdateService", arg0, arg1, arg2, arg3, arg4, arg5, arg6)
 }
 
-func (_m *MockECSClient) UpdateServiceCount(_param0 string, _param1 int64, _param2 *ecs.DeploymentConfiguration, _param3 *ecs.NetworkConfiguration, _param4 *int64) error {
-	ret := _m.ctrl.Call(_m, "UpdateServiceCount", _param0, _param1, _param2, _param3, _param4)
+func (_m *MockECSClient) UpdateServiceCount(_param0 string, _param1 int64, _param2 *ecs.DeploymentConfiguration, _param3 *ecs.NetworkConfiguration, _param4 *int64, _param5 bool) error {
+	ret := _m.ctrl.Call(_m, "UpdateServiceCount", _param0, _param1, _param2, _param3, _param4, _param5)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
-func (_mr *_MockECSClientRecorder) UpdateServiceCount(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "UpdateServiceCount", arg0, arg1, arg2, arg3, arg4)
+func (_mr *_MockECSClientRecorder) UpdateServiceCount(arg0, arg1, arg2, arg3, arg4, arg5 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "UpdateServiceCount", arg0, arg1, arg2, arg3, arg4, arg5)
 }
