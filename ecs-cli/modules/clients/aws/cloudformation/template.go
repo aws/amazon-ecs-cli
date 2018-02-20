d0486f04d14336c2918182e08c9b4339efcfdf78
Sharuzzaman Ahmat Raslan <sharuzzaman@gmail.com>
2018-02-20 18:34:37 +1100

    updated VPC ID AllowedPattern to accept long ID
    updated VPC ID AllowedPattern to accept long ID

--- ecs-cli/modules/clients/aws/cloudformation/template.go
+++ ecs-cli/modules/clients/aws/cloudformation/template.go
@@ -155,7 +155,7 @@ var template = `
       "Type": "String",
       "Description": "Optional - VPC Id of existing VPC. Leave blank to have a new VPC created",
       "Default": "",
-      "AllowedPattern": "^(?:vpc-[0-9a-f]{8}|)$",
+      "AllowedPattern": "^(?:vpc-[0-9a-f]{8,17}|)$",
       "ConstraintDescription": "VPC Id must begin with 'vpc-' or leave blank to have a new VPC created"
     },
     "SubnetIds": {
