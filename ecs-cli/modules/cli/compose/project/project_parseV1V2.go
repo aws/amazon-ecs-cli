86e4c2648ac96586817dc56a1ff98ea372960637
Stanko <stankoa@amazon.com>
2018-03-26 16:11:53 -0700

    Add correct return types to parsing functions

--- ecs-cli/modules/cli/compose/project/project_parseV1V2.go
+++ ecs-cli/modules/cli/compose/project/project_parseV1V2.go
@@ -1,14 +1,17 @@
 package project
 
 import (
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/cli/compose/containerconfig"
 	"github.com/docker/libcompose/project"
 	"github.com/sirupsen/logrus"
 )
 
-func (p *ecsProject) parseV1V2() {
+func (p *ecsProject) parseV1V2() ([]containerconfig.ContainerConfig, error) {
 	libProject := project.NewProject(&p.ecsContext.Context, nil, nil)
 	libProject.Parse()
 
 	logrus.Debug("Parsing v1/2 project...")
+
 	//TODO: convert parsed project.ServiceConfigs to ContainerConfigs
+	return []containerconfig.ContainerConfig{}, nil
 }
