4b6f997557ee079a1dfb9c543cef43c91e4490eb
Stanko <stankoa@amazon.com>
2018-03-26 14:51:07 -0700

    Add compose version check

---
+++ ecs-cli/modules/cli/compose/project/project_parseV3.go
@@ -0,0 +1,8 @@
+package project
+
+import "github.com/sirupsen/logrus"
+
+func (p *ecsProject) parseV3() {
+	// TODO: parse v3, convert ServiceConfigs to ContainerConfigs
+	logrus.Debug("Parsing v3 project...")
+}
