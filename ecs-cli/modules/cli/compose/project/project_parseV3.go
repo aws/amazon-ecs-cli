687dbfe70041556f32026ac584c17d39de242687
Stanko <stankoa@amazon.com>
2018-04-16 16:19:41 -0700

    Add compose 3 parsing, tests

--- ecs-cli/modules/cli/compose/project/project_parseV3.go
+++ ecs-cli/modules/cli/compose/project/project_parseV3.go
@@ -1,7 +1,11 @@
 package project
 
 import (
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/cli/compose/containerconfig"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 
 	"github.com/docker/cli/cli/compose/loader"
@@ -11,20 +15,67 @@ import (
 func (p *ecsProject) parseV3() (*[]containerconfig.ContainerConfig, error) {
 	logrus.Debug("Parsing v3 project...")
 
-	// TODO: parse v3, convert ServiceConfigs to ContainerConfigs
-	configDetails := types.ConfigDetails{}
+	// load and parse each compose file into a configFile
+	configFiles := []types.ConfigFile{}
+	for _, file := range p.ecsContext.ComposeFiles {
+
+		loadedFile, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
+		}
+		parsedFile, err := loader.ParseYAML(loadedFile)
+		if err != nil {
+			return nil, err
+		}
+		configFile := types.ConfigFile{
+			Filename: file,
+			Config:   parsedFile,
+		}
+		configFiles = append(configFiles, configFile)
+	}
+
+	wrkDir, err := getWorkingDir(p.ecsContext.ComposeFiles[0])
+	if err != nil {
+		return nil, err
+	}
+
+	configDetails := types.ConfigDetails{
+		WorkingDir:  wrkDir,
+		ConfigFiles: configFiles,
+		Environment: nil,
+	}
+
+	// load config from config details
 	config, err := loader.Load(configDetails)
 	if err != nil {
 		return nil, err
 	}
 
+	// convert ServiceConfigs to ContainerConfigs
+	conConfigs := []containerconfig.ContainerConfig{}
 	for _, service := range config.Services {
-		convertDockerToContainerConfig(service)
+		cCon, err := convertToContainerConfig(service)
+		if err != nil {
+			return nil, err
+		}
+		conConfigs = append(conConfigs, *cCon)
 	}
 
-	return &[]containerconfig.ContainerConfig{}, nil
+	return &conConfigs, nil
 }
 
-func convertDockerToContainerConfig(serviceConfig types.ServiceConfig) {
-	return
+func convertToContainerConfig(serviceConfig types.ServiceConfig) (*containerconfig.ContainerConfig, error) {
+	//TODO: fully convert ServiceConfig to ContainerConfig
+	c := &containerconfig.ContainerConfig{
+		Name: serviceConfig.Name,
+	}
+	return c, nil
+}
+
+func getWorkingDir(fileName string) (string, error) {
+	pwd, err := filepath.Abs(fileName)
+	if err != nil {
+		return "", errors.Wrap(err, "Unable to retrieve compose file directory")
+	}
+	return filepath.Dir(pwd), nil
 }
