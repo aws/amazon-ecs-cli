e6f900035974d1d957c8d29fdd182eaf0d4b621a
dickmao <rchiang@cs.stonybrook.edu>
2018-04-17 15:25:00 -0400

    not only update mockgen but get it internally instead of relying on user downloading it separately

---
+++ scripts/top_mockgen.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Copyright 2015-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the
+# "License"). You may not use this file except in compliance
+#  with the License. A copy of the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script wraps the mockgen tool and inserts licensing information.
+
+set -e
+declare -A MODULES
+declare -A DONE
+path=$(dirname $(realpath $0))
+cd $path/..
+for file in $(git ls-files ecs-cli/modules) ; do
+    if grep -ql "//go:generate" $file ; then
+        module=$(go list -f '{{ .ImportPath }}' ./$(dirname $file)) 
+        MODULES+=([$module]=1)
+    fi
+done
+
+# :patrik, stackoverflow
+function containsElement {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+function isAmongModules {
+    local module
+    for module in "${!MODULES[@]}" ; do
+        if [[ $1 = $module* ]] ; then
+            echo $module
+        fi
+    done
+}
+
+function tryGen {
+    local module=$1
+    if test "${DONE[$module]+isset}" ; then
+        return
+    fi
+    for import in $(go list -f '{{ join .TestImports " " }}' $module) ; do
+        IFS='/' read -r -a fn <<<${import#*$module}
+        local matched=$(isAmongModules $import)
+        if [ ! -z $matched ] && [[ $import != $matched* ]] && containsElement "mock" "${fn[@]}" ; then
+            tryGen $import
+        fi
+    done
+    PATH=$path:$PATH go generate $module
+    DONE+=([$module]=1)
+}
+
+for module in "${!MODULES[@]}" ; do
+    tryGen $module
+done
