b60bb70edce0f184bcb7f53b6f9685e2fc0d4604
Stanko <stankoa@amazon.com>
2018-04-24 09:41:02 -0700

    Update ContainerConfig to accomodate fields supplied by compose 3

--- ecs-cli/modules/cli/compose/containerconfig/containerconfig.go
+++ ecs-cli/modules/cli/compose/containerconfig/containerconfig.go
@@ -1,20 +1,32 @@
 package containerconfig
 
+import "github.com/aws/aws-sdk-go/service/ecs"
+
 // ContainerConfig all compose fields supported by the ecs-cli
 // TODO: finalize fields
 type ContainerConfig struct {
 	Name string
 
-	CapAdd      []string
-	CapDrop     []string
-	Command     string // type TBD
-	Devices     []string
-	DNS         []string // StringList in docker/cli
-	Entrypoint  string   // ShellCommand in docker/cli
-	Environment []string // MappingWithEquals in docker/cli
-	EnvFile     []string // StringList in docker/cli
-	ExtraHosts  []string
-	Hostname    string
-	HealthCheck interface{} // *HealthCheckConfig in docker/cli
-	Image       string
+	CapAdd                []string
+	CapDrop               []string
+	Command               []string
+	Devices               []string
+	DNSSearchDomains      []string
+	DNSServers            []string
+	DockerLabels          map[string]*string
+	DockerSecurityOptions []string
+	Entrypoint            []string
+	Environment           []*ecs.KeyValuePair
+	ExtraHosts            []string
+	Hostname              string
+	HealthCheck           *ecs.HealthCheck
+	Image                 string
+	Links                 []string
+	LogConfiguration      *ecs.LogConfiguration
+	PortMappings          []*ecs.PortMapping
+	Privileged            bool
+	ReadOnly              bool
+	Tmpfs                 []string
+	User                  string
+	WorkingDirectory      string
 }
