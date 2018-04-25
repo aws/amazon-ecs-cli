8c485c3f76d99878ae4cda3223d1b7135d26be2a
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-04-24 17:07:59 -0700

    Add more fields to ContainerConfig
    These correspond to docker compose fields currently supported in V1/V2

--- ecs-cli/modules/cli/compose/containerconfig/containerconfig.go
+++ ecs-cli/modules/cli/compose/containerconfig/containerconfig.go
@@ -3,14 +3,15 @@ package containerconfig
 import "github.com/aws/aws-sdk-go/service/ecs"
 
 // ContainerConfig all compose fields supported by the ecs-cli
-// TODO: finalize fields
+// TODO: finalize fields (Note: Devices and HealthCheck not currently supported)
 type ContainerConfig struct {
 	Name string
 
-	CapAdd                []string
-	CapDrop               []string
-	Command               []string
-	Devices               []string
+	CapAdd  []string
+	CapDrop []string
+	Command []string
+	CPU     int64
+	// Devices            []string
 	DNSSearchDomains      []string
 	DNSServers            []string
 	DockerLabels          map[string]*string
@@ -19,14 +20,20 @@ type ContainerConfig struct {
 	Environment           []*ecs.KeyValuePair
 	ExtraHosts            []string
 	Hostname              string
-	HealthCheck           *ecs.HealthCheck
-	Image                 string
-	Links                 []string
-	LogConfiguration      *ecs.LogConfiguration
-	PortMappings          []*ecs.PortMapping
-	Privileged            bool
-	ReadOnly              bool
-	Tmpfs                 []string
-	User                  string
-	WorkingDirectory      string
+	// HealthCheck        *ecs.HealthCheck
+	Image             string
+	Links             []string
+	LogConfiguration  *ecs.LogConfiguration
+	Memory            int64
+	MemoryReservation int64
+	MountPoints       []*ecs.MountPoint
+	PortMappings      []*ecs.PortMapping
+	Privileged        bool
+	ReadOnly          bool
+	ShmSize           int64
+	Tmpfs             []string
+	Ulimits           []*ecs.Ulimit
+	VolumesFrom       []*ecs.VolumeFrom
+	User              string
+	WorkingDirectory  string
 }
