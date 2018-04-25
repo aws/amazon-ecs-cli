6a4da690f0afa86b3e99ba7dc76472519338f1f0
Sebastian Tschan <mail@blueimp.net>
2018-04-25 22:54:52 +0200

    Simplify boolean checks.

--- ecs-cli/modules/utils/utils_test.go
+++ ecs-cli/modules/utils/utils_test.go
@@ -23,10 +23,10 @@ import (
 
 func TestInSlice(t *testing.T) {
 	list := []string{"apple", "banana", "coconut"}
-	if InSlice("apple", list) != true {
+	if !InSlice("apple", list) {
 		t.Error("Failed to find string in list.")
 	}
-	if InSlice("orange", list) != false {
+	if InSlice("orange", list) {
 		t.Error("Incorrectly found non-member string in list.")
 	}
 }
