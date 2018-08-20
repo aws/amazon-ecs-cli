2f7a14d5575a015c24b149ad4a50498d38c1b0e7
Stanko <stankoa@amazon.com>
2018-08-20 15:22:23 -0700

    Add registry-creds cmd and creds_input_reader, tests

---
+++ ecs-cli/modules/cli/regcreds/regcreds_app.go
@@ -0,0 +1,37 @@
+// Copyright 2015-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package regcreds
+
+import (
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/utils/regcreds"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+// Up creates or updates registry credential secrets and an ECS task execution role needed to use them in a task def
+func Up(c *cli.Context) {
+	args := c.Args()
+
+	if len(args) != 1 {
+		log.Fatal("Exactly 1 credential file is required. Found: ", len(args))
+	}
+
+	credsInput, err := readers.ReadCredsInput(args[0])
+	if err != nil {
+		log.Fatal("Error executing 'up': ", err)
+	}
+	log.Infof("Read creds input: %+v", credsInput) // remove after SDK calls added
+
+	//TODO: create secrets, create role, produce output
+}
