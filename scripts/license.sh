d741ace89f4cce1d716f6c37026ae256d2afecc6
Wesley Pettit <wppttt@amazon.com>
2019-02-04 18:46:49 -0800

    Update license year to 2019

--- scripts/license.sh
+++ scripts/license.sh
@@ -53,7 +53,7 @@ for repo in ./../../../vendor/gopkg.in/*; do
 done;
 
 cat << EOF > "${outputfile}"
-// Copyright 2015-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2015-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
