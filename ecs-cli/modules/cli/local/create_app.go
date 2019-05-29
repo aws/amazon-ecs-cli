c9e590903f00367658ad9d9ecc426a569566d571
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-05-29 15:41:43 -0700

    Write to docker-compose.local.yml with confirmation

--- ecs-cli/modules/cli/local/create_app.go
+++ ecs-cli/modules/cli/local/create_app.go
@@ -16,14 +16,76 @@
 package local
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/service/ecs"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
+const (
+	LocalOutFileName = "./docker-compose.local.yml"
+	LocalOutFileMode = os.FileMode(0644) // Owner=read/write, Other=readonly
+)
+
 func Create(c *cli.Context) {
-	// 1. read in task def (from file or arn)
-	// 2. parse task def into go object
-	// 3. write to docker-compose.local.yml file
-	fmt.Println("foo") // placeholder
+	// 1. Read in task definition (from file or ARN)
+	filename := "./task-definition.json" // FIXME defaults
+
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Error reading task definition from %v", filename)
+	}
+
+	// 2. Parse task def into go object
+	taskDefinition := ecs.TaskDefinition{}
+	json.Unmarshal(bytes, &taskDefinition)
+
+	// 3. Convert to docker compose
+	// fmt.Printf("TASK DEF: %+v", taskDefinition)
+
+	// 4. Write to docker-compose.local.yml file
+
+	data := []byte("taskDefinition")
+
+	err = writeLocal(data)
+	if err != nil {
+		log.Fatalf("Error with local create: %s", err.Error())
+	}
+
+}
+
+func writeLocal(data []byte) error {
+	// Will error if the file already exists, otherwise create
+	out, err := os.OpenFile(LocalOutFileName, os.O_WRONLY|os.O_CREATE|os.O_EXCL, LocalOutFileMode)
+	defer out.Close()
+
+	if err != nil {
+		fmt.Println("docker-compose.local.yml file already exists. Do you want to write over this file? [y/N]")
+
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("Error reading input: %s", err.Error())
+		}
+
+		formattedInput := strings.ToLower(strings.TrimSpace(input))
+
+		if formattedInput != "yes" && formattedInput != "y" {
+			return fmt.Errorf("Aborted writing compose file. To retry, rename or move %s", LocalOutFileName) // TODO add force flag
+		}
+
+		// Overwrite local compose file
+		err = ioutil.WriteFile(LocalOutFileName, data, LocalOutFileMode)
+		return err
+	}
+
+	_, err = out.Write(data)
+
+	return err
 }
