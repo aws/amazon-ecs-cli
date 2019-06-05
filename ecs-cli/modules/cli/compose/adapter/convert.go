6bd847056b74594a3d0718cc42c223389bbbb039
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-06-04 17:53:34 -0700

    Fix license, add package comment

--- ecs-cli/modules/cli/compose/adapter/convert.go
+++ ecs-cli/modules/cli/compose/adapter/convert.go
@@ -1,4 +1,4 @@
-// Copyright 2015-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2015-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
@@ -11,6 +11,7 @@
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+// Package adapater converts docker compose objects into ECS objects
 package adapter
 
 import (
