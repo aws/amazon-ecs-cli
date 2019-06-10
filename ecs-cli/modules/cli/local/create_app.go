c8a8dfb1cbc9f5cfdc08dc816104dd2958a6c1c5
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-06-09 19:51:14 -0700

    Refactor to encapsulate read/write/conversion logic into interface
    - Create LocalProject interface, which holds the cli.Context
    - Acounts for -o flag for specifying custom docker compose file to
    write to

--- ecs-cli/modules/cli/local/create_app.go
+++ ecs-cli/modules/cli/local/create_app.go
@@ -16,153 +16,42 @@
 package local
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
 
-	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/cli/local/converter"
-	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/commands/flags"
-	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/config"
-	ecsclient "github.com/aws/amazon-ecs-cli/ecs-cli/modules/clients/aws/ecs"
-	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/cli/local/project"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
-const (
-	LocalOutFileName = "./docker-compose.local.yml"
-	LocalOutFileMode = os.FileMode(0644) // Owner=read/write, Other=readonly
-	LocalInFileName = "./task-definition.json"
-)
-
 func Create(c *cli.Context) {
-	err := createLocal(c)
+	project := localproject.New(c)
+
+	err := createLocal(project)
 	if err != nil {
 		log.Fatalf("Error with local create: %s", err.Error())
 	}
 
-	fmt.Printf("Successfully created %s\n", LocalOutFileName)
+	fmt.Printf("Successfully wrote %s\n", project.LocalOutFileName())
 }
 
-func createLocal(c *cli.Context) error {
-	// Read task definition (from file or ARN)
-	// returns ecs.TaskDefinition
-	taskDefinition, err := readTaskDefinition(c)
+func createLocal(project localproject.LocalProject) error {
+	// Reads task definition and loads it onto project
+	err := project.ReadTaskDefinition()
 	if err != nil {
 		return err
 	}
 
-	// Convert to docker compose
-	data, err := convertLocal(taskDefinition)
+	// Converts Task definition and loads onto project
+	err = project.Convert()
 	if err != nil {
 		return err
 	}
 
-	// Write to docker-compose.local.yml file
-	err = writeLocal(data)
+	// Write to local output file
+	err = project.Write()
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-func readTaskDefinitionFromFile(filename string) (*ecs.TaskDefinition, error) {
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading task definition from %s: %s", filename, err.Error())
-	}
-
-	taskDefinition := ecs.TaskDefinition{}
-	err = json.Unmarshal(bytes, &taskDefinition)
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing task definition JSON: %s", err.Error())
-	}
-
-	return &taskDefinition, nil
-}
-
-var newCommandConfig = func(context *cli.Context, rdwr config.ReadWriter) (*config.CommandConfig, error) {
-	return config.NewCommandConfig(context, rdwr)
-}
-
-// FIXME: NOTE this will actually read from either ARN or Task Definition family name
-func readTaskDefinitionFromArn(arn string, c *cli.Context) (*ecs.TaskDefinition, error) {
-	rdwr, err := config.NewReadWriter()
-	if err != nil {
-		return nil, err
-	}
-
-	commandConfig, err := newCommandConfig(c, rdwr)
-	if err != nil {
-		return nil, err
-	}
-
-	ecsClient := ecsclient.NewECSClient(commandConfig)
-
-	return ecsClient.DescribeTaskDefinition(arn)
-}
-
-func readTaskDefinition(c *cli.Context) (*ecs.TaskDefinition, error) {
-	arn := c.String(flags.TaskDefinitionArnFlag)
-	filename := c.String(flags.TaskDefinitionFileFlag)
-
-	if arn != "" && filename != "" {
-		return nil, fmt.Errorf("Cannot specify both --%s and --%s flags.", flags.TaskDefinitionArnFlag, flags.TaskDefinitionFileFlag)
-	}
-
-	if arn != "" {
-		return readTaskDefinitionFromArn(arn, c)
-	}
-
-	if filename != "" {
-		return readTaskDefinitionFromFile(filename)
-	}
-
-	// Try reading local task-definition.json file
-	if _, err := os.Stat(LocalInFileName); err == nil {
-		return readTaskDefinitionFromFile(LocalInFileName)
-	}
-
-	return nil, fmt.Errorf("Could not detect valid Task Definition")
-}
-
-func convertLocal(taskDefinition *ecs.TaskDefinition) ([]byte, error) {
-	data, err := converter.ConvertToDockerCompose(taskDefinition)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-func writeLocal(data []byte) error {
-	// Will error if the file already exists, otherwise create
-	out, err := os.OpenFile(LocalOutFileName, os.O_WRONLY|os.O_CREATE|os.O_EXCL, LocalOutFileMode)
-	defer out.Close()
-
-	if err != nil {
-		fmt.Println("docker-compose.local.yml file already exists. Do you want to write over this file? [y/N]")
-
-		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			return fmt.Errorf("Error reading input: %s", err.Error())
-		}
-
-		formattedInput := strings.ToLower(strings.TrimSpace(input))
-
-		if formattedInput != "yes" && formattedInput != "y" {
-			return fmt.Errorf("Aborted writing compose file. To retry, rename or move %s", LocalOutFileName) // TODO add force flag
-		}
-
-		// Overwrite local compose file
-		err = ioutil.WriteFile(LocalOutFileName, data, LocalOutFileMode)
-		return err
-	}
-
-	_, err = out.Write(data)
-
-	return err
 }
---
+++ ecs-cli/modules/cli/local/project/project.go
@@ -0,0 +1,195 @@
+// Copyright 2015-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Package localproject defines LocalProject interface and implements them on localProject
+
+package localproject
+
+import (
+
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/cli/local/converter"
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/config"
+	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/commands/flags"
+	ecsclient "github.com/aws/amazon-ecs-cli/ecs-cli/modules/clients/aws/ecs"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/urfave/cli"
+)
+
+const (
+	LocalOutDefaultFileName = "./docker-compose.local.yml"
+	LocalOutFileMode = os.FileMode(0644) // Owner=read/write, Other=readonly
+	LocalInFileName = "./task-definition.json"
+)
+
+type LocalProject interface {
+	ReadTaskDefinition() error
+	Convert() error
+	Write() error
+	LocalOutFileName() string
+}
+
+type localProject struct {
+	context        *cli.Context
+	taskDefinition *ecs.TaskDefinition
+	localBytes     []byte
+	localOutFileName string
+}
+
+func New(context *cli.Context) LocalProject {
+	p := &localProject{ context: context }
+	return p
+}
+
+func (p *localProject) TaskDefinition() *ecs.TaskDefinition {
+	return p.taskDefinition
+}
+
+func (p *localProject) LocalOutFileName() string {
+	return p.localOutFileName
+}
+
+func (p *localProject) ReadTaskDefinition() error {
+	arn := p.context.String(flags.TaskDefinitionArnFlag)
+	filename := p.context.String(flags.TaskDefinitionFileFlag)
+
+	if arn != "" && filename != "" {
+		return fmt.Errorf("Cannot specify both --%s and --%s flags.", flags.TaskDefinitionArnFlag, flags.TaskDefinitionFileFlag)
+	}
+
+	var taskDefinition *ecs.TaskDefinition
+	var err error
+
+	if arn != "" {
+		taskDefinition, err = p.readTaskDefinitionFromArn(arn)
+		if err != nil {
+			return err
+		}
+	}
+
+	if filename != "" {
+		taskDefinition, err = p.readTaskDefinitionFromFile(filename)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Try reading local task-definition.json file by default
+	if _, err := os.Stat(LocalInFileName); err == nil {
+		taskDefinition, err = p.readTaskDefinitionFromFile(LocalInFileName)
+		if err != nil {
+			return err
+		}
+	}
+
+	if taskDefinition == nil {
+		return fmt.Errorf("Could not detect valid Task Definition")
+	}
+
+	p.taskDefinition = taskDefinition
+	return nil
+}
+
+func (p *localProject) readTaskDefinitionFromFile(filename string) (*ecs.TaskDefinition, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading task definition from %s: %s", filename, err.Error())
+	}
+
+	taskDefinition := ecs.TaskDefinition{}
+	err = json.Unmarshal(bytes, &taskDefinition)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing task definition JSON: %s", err.Error())
+	}
+
+	return &taskDefinition, nil
+}
+
+var newCommandConfig = func(context *cli.Context, rdwr config.ReadWriter) (*config.CommandConfig, error) {
+	return config.NewCommandConfig(context, rdwr)
+}
+
+// FIXME: NOTE this will actually read from either ARN or Task Definition family name
+func (p *localProject) readTaskDefinitionFromArn(arn string) (*ecs.TaskDefinition, error) {
+	rdwr, err := config.NewReadWriter()
+	if err != nil {
+		return nil, err
+	}
+
+	commandConfig, err := newCommandConfig(p.context, rdwr)
+	if err != nil {
+		return nil, err
+	}
+
+	ecsClient := ecsclient.NewECSClient(commandConfig)
+
+	return ecsClient.DescribeTaskDefinition(arn)
+}
+
+func (p *localProject) Convert() (error) {
+	// get secrets here
+	data, err := converter.ConvertToDockerCompose(p.taskDefinition)
+
+	if err != nil {
+		return err
+	}
+
+	p.localBytes = data
+
+	return nil
+}
+
+func (p *localProject) Write() (error) {
+	// Will error if the file already exists, otherwise create
+
+	outputFileName := p.context.String(flags.LocalOutputFlag)
+	if outputFileName == "" {
+		outputFileName = LocalOutDefaultFileName
+	}
+	p.localOutFileName = outputFileName
+
+	out, err := os.OpenFile(outputFileName, os.O_WRONLY|os.O_CREATE|os.O_EXCL, LocalOutFileMode)
+	defer out.Close()
+
+	data := p.localBytes
+
+	if err != nil {
+		fmt.Printf("%s file already exists. Do you want to write over this file? [y/N]\n", outputFileName)
+
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("Error reading input: %s", err.Error())
+		}
+
+		formattedInput := strings.ToLower(strings.TrimSpace(input))
+
+		if formattedInput != "yes" && formattedInput != "y" {
+			return fmt.Errorf("Aborted writing compose file. To retry, rename or move %s", outputFileName) // TODO add force flag
+		}
+
+		// Overwrite local compose file
+		err = ioutil.WriteFile(outputFileName, data, LocalOutFileMode)
+		return err
+	}
+
+	_, err = out.Write(data)
+
+	return err
+}
