ade2b08221b8ccc6e0e7f94e1ecd5b4103e48464
penghaoh <penghaoh@amazon.com>
2019-07-01 14:23:26 -0700

    added print out task definition revision

--- ecs-cli/modules/cli/local/localproject/project.go
+++ ecs-cli/modules/cli/local/localproject/project.go
@@ -27,6 +27,7 @@ import (
 	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/clients/aws/secretsmanager"
 	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/commands/flags"
 	"github.com/aws/amazon-ecs-cli/ecs-cli/modules/config"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/urfave/cli"
 )
@@ -105,6 +106,7 @@ func (p *localProject) ReadTaskDefinition() error {
 
 	if remote != "" {
 		taskDefinition, err = p.readTaskDefinitionFromRemote(remote)
+		fmt.Printf("Reading task definition from %s:%v\n", aws.StringValue(taskDefinition.Family), aws.Int64Value(taskDefinition.Revision))
 		if err != nil {
 			return err
 		}
