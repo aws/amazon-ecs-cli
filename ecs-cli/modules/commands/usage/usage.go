1d2997ea4d4d9db4419e40196d5a599c9520eff4
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-07-17 15:59:20 -0700

    Make tense for usage strings consistent

--- ecs-cli/modules/commands/usage/usage.go
+++ ecs-cli/modules/commands/usage/usage.go
@@ -50,7 +50,7 @@ const (
 )
 
 // Compose Service
-const(
+const (
 	Service       = "Manage Amazon ECS services with docker-compose-style commands on an ECS cluster."
 	ServiceCreate = "Creates an ECS service from your compose file. The service is created with a desired count of 0, so no containers are started by this command. Note that we do not recommend using plain text environment variables for sensitive information, such as credential data."
 	ServiceStart  = "Starts one copy of each of the containers on an existing ECS service by setting the desired count to 1 (only if the current desired count is 0)."
@@ -72,22 +72,21 @@ const (
 
 // Image
 const (
-	Push   = "Push an image to an Amazon ECR repository."
-	Pull   = "Pull an image from an Amazon ECR repository."
-	Images = "List images an Amazon ECR repository."
+	Push   = "Pushes an image to an Amazon ECR repository."
+	Pull   = "Pulls an image from an Amazon ECR repository."
+	Images = "Lists images an Amazon ECR repository."
 )
 
 // License
-const(
+const (
 	License = "Prints the LICENSE files for the ECS CLI and its dependencies."
 )
 
 // Local
 var (
-	Local = "Run your ECS tasks locally."
-	LocalCreate ="Create a Compose file from an ECS task definition."
-	LocalUp = fmt.Sprintf("Run containers locally from an ECS Task Definition. NOTE: Creates a docker-compose file in current directory and a %s if one doesn't exist. ", network.EcsLocalNetworkName)
-	LocalPs = "List locally running ECS task containers."
-	LocalDown = fmt.Sprintf("Stop and remove a running ECS task. NOTE: Removes the %s if it has no more running tasks. ", network.EcsLocalNetworkName)
+	Local       = "Runs your ECS tasks locally."
+	LocalCreate = "Creates a Compose file from an ECS task definition."
+	LocalUp     = fmt.Sprintf("Runs containers locally from an ECS Task Definition. NOTE: Creates a docker-compose file in current directory and a %s if one doesn't exist. ", network.EcsLocalNetworkName)
+	LocalPs     = "Lists locally running ECS task containers."
+	LocalDown   = fmt.Sprintf("Stops and removes a running ECS task. NOTE: Removes the %s if it has no more running tasks. ", network.EcsLocalNetworkName)
 )
-
