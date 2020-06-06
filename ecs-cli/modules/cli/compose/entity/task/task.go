132b9185e052ba580edec1051c1dda3a52fb071b
Tatsuya Ogawa <togawa83@gmail.com>
2020-06-06 17:32:10 +0900

    Add EFS volume support to ECS Params for task.

--- ecs-cli/modules/cli/compose/entity/task/task.go
+++ ecs-cli/modules/cli/compose/entity/task/task.go
@@ -382,6 +382,14 @@ func (t *Task) buildRunTaskInput(taskDefinition string, count int, overrides map
 		}
 	}
 
+	// TODO: revert to "LATEST" when latest refers to 1.4.0
+	if len(ecsParams.TaskDefinition.EFSVolumes) != 0 {
+		log.Warnf("Detected an EFS Volume in task definition %s", taskDefinition)
+		log.Warn("Using Fargate platform version 1.4.0, which includes changes to the networking flows for VPC endpoint customers.")
+		log.Warn("Learn more: https://aws.amazon.com/blogs/containers/aws-fargate-launches-platform-version-1-4/")
+		runTaskInput.PlatformVersion = aws.String("1.4.0")
+	}
+
 	return runTaskInput, nil
 }
 
